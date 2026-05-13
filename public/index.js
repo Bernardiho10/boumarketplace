4a2a6f661177dfb833abd69f5f7b5dc0e86ffa94
Bernard Oko <bernardarikuoko@gmail.com>
2026-05-13 10:13:21 +0100

    Fixed the Ninja: element #ninja-verify-container not found

--- public/index.js
+++ public/index.js
@@ -598,8 +598,8 @@ function initNinjaIntegration(retries = 20) {
     }
     const config = {
         apiKey: NINJA_CONFIG.CLIENT_KEY,
-        targetElement: "#ninja-verify-container",
-        display: "button",
+        targetElement: "#ninja-container",
+        display: "form",
         buttonLabel: "Verify Identity",
         idType: "nin",
         mode: "lookup",
